9dc51e48a75571a5487ffccda92c432b34de786a
Alexandre BISIAUX <alexandre_bisiaux@connect-tech.sncf>
2025-10-07 09:28:36 +0200

    improve swiper

--- src/components/EventCarousel.tsx
+++ src/components/EventCarousel.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@components/Button.tsx'
 import type { Evenement } from '@models/Evenement.ts'
 import { Calendar, MapPin } from '@phosphor-icons/react'
+import { useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Navigation, Pagination } from 'swiper/modules'
+import type { Swiper as SwiperType } from 'swiper'
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 type Props = {
@@ -11,10 +13,12 @@ type Props = {
 
 export default function EventCarousel({ events }: Props) {
   const navigate = useNavigate()
+  const swiperRef = useRef<SwiperType>(null)
 
-  const hasTime = (date: Date) => date.getHours() !== 0 ||
+  const hasTime = (date: Date) =>
+    date.getHours() !== 0 ||
     date.getMinutes() !== 0 ||
-    date.getSeconds()!== 0 ||
+    date.getSeconds() !== 0 ||
     date.getMilliseconds() !== 0
 
   const isSameDay = (start: Date, end: Date) =>
@@ -23,93 +27,116 @@ export default function EventCarousel({ events }: Props) {
     start.getDate() === end.getDate()
 
   return (
-    <Swiper
-      modules={[Navigation, Pagination]}
-      slidesPerView={1}
-      slidesPerGroup={1}
-      navigation
-      pagination={{ clickable: true }}
+    <div
       className="h-full"
-      autoplay={true}
+      onMouseEnter={() => swiperRef.current?.autoplay?.stop()}
+      onMouseLeave={() => swiperRef.current?.autoplay?.start()}
     >
-      {events.map((event) => {
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        slidesPerGroup={1}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        className="h-full"
+        onSwiper={(swiper) => (swiperRef.current = swiper)} // 🔹 Stocke l’instance du Swiper
+      >
+        {events.map((event) => {
+          const startDate = new Date(event.date_heure_debut)
+          const endDate = new Date(event.date_heure_fin)
 
-        const startDate = new Date(event.date_heure_debut)
-        const endDate = new Date(event.date_heure_fin)
+          return (
+            <SwiperSlide key={event.id} className="h-full ">
+              <div className="flex h-full">
+                <div className="flex flex-grow bg-carousel rounded-2xl mx-10 lg:mx-20 my-10">
+                  <div className="hidden lg:flex items-center">
+                    <img
+                      src={event.thumbnail.url}
+                      alt={event.titre}
+                      className="h-full pl-5 py-5 rounded-l-2xl object-cover"
+                    />
+                  </div>
+                  <div className="flex-1 flex-grow py-2 lg:py-5 px-2 lg:px-5 text-primary items-center">
+                    <div className="h-full px-2 py-2">
+                      <h2 className="text-center lg:text-left text-2xl lg:text-4xl font-semibold mb-2 text-secondary">
+                        {event.titre}
+                      </h2>
 
-        return (
-          <SwiperSlide key={event.id} className="h-full">
-            <div className="flex h-full ">
-              <div className="flex flex-grow bg-carousel rounded-2xl mx-10 lg:mx-20 my-10">
-                <div className="hidden lg:flex items-center">
-                  <img
-                    src={event.thumbnail.url}
-                    alt={event.titre}
-                    className="h-full pl-5 py-5"
-                  />
-                </div>
-                <div className="flex-1 flex-grow py-2 lg:py-5 px-2 lg:px-5 text-primary items-center">
-                  <div className="h-full px-2 py-2">
-                    <h2 className="text-center lg:text-left text-2xl lg:text-4xl font-semibold mb-2 text-secondary">
-                      {event.titre}
-                    </h2>
-
-                    <div className="flex flex-col xl:flex-row gap-2 mb-4 items-center lg:items-start">
-                      <div className="flex">
-                        <Calendar
-                          className="inline text-tertiary pr-2"
-                          size={24}
-                        />
-                        <span className="align-top text-white">
-                          {startDate.toLocaleString(
-                            'fr-FR',
-                            { dateStyle: 'short', timeStyle: hasTime(startDate) ? 'short' : undefined },
+                      <div className="flex flex-col xl:flex-row gap-2 mb-4 items-center lg:items-start">
+                        <div className="flex">
+                          <Calendar
+                            className="inline text-tertiary pr-2"
+                            size={24}
+                          />
+                          <span className="align-top text-white">
+                            {startDate.toLocaleString('fr-FR', {
+                              dateStyle: 'short',
+                              timeStyle: hasTime(startDate)
+                                ? 'short'
+                                : undefined,
+                            })}
+                          </span>
+                          {event.date_heure_fin && (
+                            <>
+                              <span className="pl-1 align-top text-white">
+                                {'-'}
+                              </span>
+                              <span className="pl-1 align-top text-white">
+                                {endDate.toLocaleString('fr-FR', {
+                                  dateStyle: !isSameDay(startDate, endDate)
+                                    ? 'short'
+                                    : undefined,
+                                  timeStyle: hasTime(endDate)
+                                    ? 'short'
+                                    : undefined,
+                                })}
+                              </span>
+                            </>
                           )}
-                      </span>
-                        {event.date_heure_fin && (
-                          <span className="pl-1 align-top text-white">
-                          {'- '}
-                            {endDate.toLocaleString(
-                              'fr-FR',
-                              { dateStyle: !isSameDay(startDate, endDate) ? 'short' : undefined, timeStyle: hasTime(endDate) ? 'short' : undefined },
-                            )}
-                        </span>
+                        </div>
+                        <div className="flex">
+                          <MapPin
+                            className="inline text-tertiary pr-2"
+                            size={24}
+                          />
+                          <span className="align-top text-white">
+                            {event.lieu}
+                          </span>
+                        </div>
+                      </div>
+
+                      <div className="hidden md:block text-center lg:text-left">
+                        <p className="md:max-h-20 text-sm mb-2 leading-relaxed text-white multi-truncate">
+                          {event.description}
+                        </p>
+                      </div>
+
+                      <div className="flex justify-center lg:justify-start">
+                        {(event.actualite || event.lien_bouton) && (
+                          <Button
+                            label={event.titre_bouton || 'En savoir plus'}
+                            onClick={() => {
+                              if (event.lien_bouton) {
+                                window.open(event.lien_bouton, '_blank')
+                              } else if (event.actualite) {
+                                navigate(
+                                  `/actualites/${event.actualite.documentId}`
+                                )
+                              }
+                            }}
+                          />
                         )}
                       </div>
-                      <div className="flex">
-                        <MapPin className="inline text-tertiary pr-2" size={24} />
-                        <span className="align-top text-white">{event.lieu}</span>
-                      </div>
-                    </div>
-
-                    <div className="hidden md:block text-center lg:text-left">
-                      <p className="md:max-h-20 text-sm mb-2 leading-relaxed text-white multi-truncate">
-                        {event.description}
-                      </p>
-                    </div>
-
-                    <div className="flex justify-center lg:justify-start">
-                      {(event.actualite || event.lien_bouton) && (
-                        <Button
-                          label={event.titre_bouton || 'En savoir plus'}
-                          onClick={() => {
-                            if (event.lien_bouton) {
-                              window.open(event.lien_bouton, '_blank')
-                            } else if (event.actualite) {
-                              navigate(`/actualites/${event.actualite.documentId}`)
-                            }
-                          }
-                          }
-                        />
-                      )}
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-        )
-      })}
-    </Swiper>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </div>
   )
 }
